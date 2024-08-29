10c26f8632f27dc591301ca21c3a671be6baa567
Vadym Zhydenko <zhydenkodeveloper@gmail.com>
2024-08-29 23:22:43 +0200

    Initial Commit

---
+++ Test-task-6c4fcd94cbdf4d519f29d0c0748e5b89/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Test-task-6c4fcd94cbdf4d519f29d0c0748e5b89
+//
+//  Created by Personal on 29/08/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Test-task-6c4fcd94cbdf4d519f29d0c0748e5b89/Test_task_6c4fcd94cbdf4d519f29d0c0748e5b89App.swift
@@ -0,0 +1,17 @@
+//
+//  Test_task_6c4fcd94cbdf4d519f29d0c0748e5b89App.swift
+//  Test-task-6c4fcd94cbdf4d519f29d0c0748e5b89
+//
+//  Created by Personal on 29/08/2024.
+//
+
+import SwiftUI
+
+@main
+struct Test_task_6c4fcd94cbdf4d519f29d0c0748e5b89App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
